3083edf22672f96ad183a934bc9ee63e97ffb96d
EkaterinaLapunova <katyalapunova96@gmail.com>
2021-08-09 12:59:14 +0300

    Create Modal element

---
+++ vue-sorter/src/components/modal/Modal.vue
@@ -0,0 +1,116 @@
+<template>
+  <div class="modal-back">
+    <div class="modal-island">
+      <div class="modal-island__header">
+        <h4>How many people?</h4>
+        <button class="modal-island__btn-close btn-close"></button>
+      </div>
+      <div class="modal-island__main">
+        <form action="" class="modal-island__form"
+              v-on:submit.prevent
+              >
+          <label for="peopleNumber">
+            Enter a number of how many people you want to add to the list
+          </label>
+          <input class="modal-island__input" id="peopleNumber" type="number" min="20" max="100">
+          <div class="modal-island__btn-section">
+            <button class="modal-island__btn-reset btn btn-light" type="reset">Reset</button>
+            <button class="modal-island__btn-confirm btn btn-warning" type="submit">Start</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Modal',
+  data() {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  .modal-back {
+    z-index: 1;
+
+    position: absolute;
+    top: 0;
+    left: 0;
+
+    display: flex;
+
+    width: 100%;
+    height: 100%;
+
+    background: rgba(255, 255, 255, 0.3);
+  }
+
+  .modal-island {
+    z-index: 2;
+
+    position: relative;
+
+    margin: auto;
+    align-self: center;
+
+    width: 500px;
+    height: 250px;
+
+    background: white;
+
+    border-radius: 4px;
+
+    box-shadow: 0 2px 3px 2px lightgray;
+
+    &__header {
+      display: flex;
+      justify-content: space-between;
+
+      padding: 5px;
+    }
+
+    &__main {
+      border-top: 2px solid lightgray;
+
+      padding: 20px;
+    }
+
+    label {
+      font-weight: 500;
+
+      margin-top: 20px;
+    }
+
+    &__input {
+      margin-top: 15px;
+      text-align: center;
+
+      width: 455px;
+
+      border: 1px solid lightgray;
+      border-radius: 3px;
+    }
+
+    &__btn-section {
+      position: absolute;
+      bottom: 0;
+      left: 0;
+
+      width: 100%;
+      text-align: right;
+
+      border-top: 2px solid lightgray;
+
+      & > button {
+        margin: 10px;
+
+        width: 100px;
+      }
+    }
+  }
+</style>
--- vue-sorter/src/views/VueSorter.vue
+++ vue-sorter/src/views/VueSorter.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="vue-sorter">
+    <Modal />
     <img class="vue-logo" alt="Vue logo" src="../assets/logo.png">
     <Header />
     <Main />
@@ -9,12 +10,14 @@
 <script>
 import Header from '@/components/header/Header.vue'
 import Main from '@/components/main/Main.vue'
+import Modal from '@/components/modal/Modal.vue'
 
 export default {
   name: 'VueSorter',
   components: {
     Header,
     Main,
+    Modal
   }
 }
 
