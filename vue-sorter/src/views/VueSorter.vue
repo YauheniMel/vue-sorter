46a1cff5aa6e81afa19603f1336786ed5da915c1
EkaterinaLapunova <katyalapunova96@gmail.com>
2021-08-09 09:05:30 +0300

    Create initial vue project

---
+++ vue-sorter/src/views/VueSorter.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="vue-sorter">
+    <img alt="Vue logo" src="../assets/logo.png">
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'VueSorter',
+  components: {
+  }
+}
+
+</script>
